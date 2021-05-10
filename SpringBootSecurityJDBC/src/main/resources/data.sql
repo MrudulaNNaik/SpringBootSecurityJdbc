ba8aea583e9ea76111127146693996d003d5c12c
MRUDULA N NAIK <mrudulannaik1999@gmail.com>
2021-05-10 16:21:00 +0530

    initial commit

---
+++ SpringBootSecurityJDBC/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO users(username,password,enabled)
+values('mrudula','naik',true);
+
+INSERT INTO users(username,password,enabled)
+values('lata','gdn',true);
+
+INSERT INTO authorities(username,authority)
+values('mrudula','ROLE_USER');
+
+INSERT INTO authorities(username,authority)
+values('lata','ROLE_ADMIN');
+
